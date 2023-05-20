8e77617ef65576b57b3cea592ec49a8da4e78752
montekrist0 <bogdanovigor1994@mail.ru>
2023-05-20 13:39:33 +0300

    update dict to dataclass in parse_object divide cmd_handler and elasticseeker classes

--- voice_helper_app/src/services/cmd_handler.py
+++ voice_helper_app/src/services/cmd_handler.py
@@ -6,41 +6,35 @@ from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from core.configs import settings
 from db.clients.mongo import get_mongo_client
-from db.clients.elastic import get_elastic
-from elasticsearch import AsyncElasticsearch
-from services.movies_storage_handler import ElasticSeeker
+
 from fastapi import Depends
+from services.models.models import UserQueryObject
 
 
 class CommandHandler:
-    def __init__(self, commands_db: AsyncIOMotorDatabase, es_client: AsyncElasticsearch):
+    def __init__(self, commands_db: AsyncIOMotorDatabase):
         self.db = commands_db
         self.last_update_cmd_tbr: datetime | None = None
         self.commands: dict | None = None
         self.to_be_removed: list | None = None
-        self.movies_db = ElasticSeeker(es_client)
+        # self.movies_db = ElasticSeeker(es_client)
 
-    async def handle_user_query(self, user_txt: str):
+    async def handle_user_query(self, user_txt: str) -> UserQueryObject:
         """Входная функция. Создается рабочий объект(словарь). С основной информацией для парсинга данных"""
-        parse_object = {
-            'before_cleaning_user_txt': user_txt,
-            'after_cleaning_user_txt': '',
-            'discovered_cmd': '',
-            'final_cmd': '',
-            'original_txt': '',
-            'key_word': '',
-            'answer': '',
-            'percent': 0,
-        }
+        user_query_object = UserQueryObject()
         await self.update_cmd_tbr()
-        parse_object['after_cleaning_user_txt'] = self.cleaning_user_txt(user_txt)
-        parse_object = self.recognize_cmd(parse_object)
-        parse_object = self.recognize_key_word(parse_object)
-        print(parse_object)
-        parse_object = await self.execute_cmd(parse_object)
-        return parse_object['answer']
+        user_query_object.after_cleaning_user_txt = self.cleaning_user_txt(user_txt)
+        self.recognize_cmd(user_query_object)
+        self.recognize_key_word(user_query_object)
+        # TODO: везде в проекте заменить print на DEBUG логирование
+        print(user_query_object)
+        return user_query_object
+        # user_query_object = await self.execute_cmd(user_query_object)
+        # return user_query_object.answer
 
     async def update_cmd_tbr(self):
+        # TODO: выяснить нужна ли эта функция и можно ли как-то
+        #       оптимизировать этот функционал?
         """Вызов получения данных(cmd, tbr) через дельту времени"""
         delta_update = timedelta(hours=settings.delta_update_cmd_tbr)
         if self.last_update_cmd_tbr is None or self.last_update_cmd_tbr + delta_update < datetime.utcnow():
@@ -56,6 +50,7 @@ class CommandHandler:
         self.commands = commands
 
     async def get_actual_tbr(self):
+        # TODO: что такое tbr? совсем непонятно. надо изменить название
         """Получение списка слов для первичной чистки запроса пользователя"""
         collection = self.db[settings.mongo_collection_tbr]
         tbr = await collection.find_one()
@@ -64,68 +59,42 @@ class CommandHandler:
 
     def cleaning_user_txt(self, user_txt: str) -> str:
         """Первичная очистка запроса пользователя от фонового шума"""
-        return ' '.join([word.lower() for word in user_txt.split() if word.lower() not in self.to_be_removed]).strip()
+        return ' '.join([word for word in user_txt.lower().split() if word not in self.to_be_removed]).strip()
 
-    def recognize_cmd(self, parse_object: dict) -> dict:
+    def recognize_cmd(self, user_query_object: UserQueryObject) -> UserQueryObject:
         """Обнаружение команды используя расстояние Левенштейна"""
         for command_name, texts_comparisons in self.commands.items():
             for original_text in texts_comparisons:
-                percent = fuzz.ratio(parse_object['after_cleaning_user_txt'], original_text)
-                if percent > parse_object['percent']:
-                    parse_object['discovered_cmd'] = command_name
-                    parse_object['final_cmd'] = command_name
-                    parse_object['original_txt'] = original_text
-                    parse_object['percent'] = percent
-        if parse_object['percent'] <= 49:
-            parse_object['final_cmd'] = 'unknown'
-        if 50 <= parse_object['percent'] < 60:
-            parse_object['final_cmd'] = 'ask_again'
-        return parse_object
 
-    async def execute_cmd(self, parse_object: dict) -> dict:
-        """Выполнение команды, запрос в еластик"""
-        command_matrix = {
-            'author': self.movies_db.get_film_author,
-            'actor': self.movies_db.get_actor_films,
-            'how_many_films': self.movies_db.get_director_films_count,
-            'time_film': self.movies_db.get_film_length,
-            'top_films': self.movies_db.get_top_films,
-            'top_films_genre': self.movies_db.get_top_n_films_in_genre,
-            'top_films_person': self.movies_db.get_actor_top_n_films,
-            'film_genre': self.movies_db.get_film_genre,
-            'top_actor': self.movies_db.get_top_actor,
-            'film_about': self.movies_db.get_film_description,
-            'film_year': self.movies_db.get_film_year,
-            'film_rating': self.movies_db.get_film_rating
-        }
+                percent = fuzz.ratio(user_query_object.after_cleaning_user_txt, original_text)
+                if percent > user_query_object.percent:
+                    user_query_object.discovered_cmd = command_name
+                    user_query_object.final_cmd = command_name
+                    user_query_object.original_txt = original_text
+                    user_query_object.percent = percent
 
-        command = parse_object.get('final_cmd')
-        keyword = parse_object.get('key_word')
-        answer = 'К сожалению, команда не распознана... пожалуйста, повторите запрос'
+        if user_query_object.percent <= 49:
+            user_query_object.final_cmd = 'unknown'
 
-        if command in command_matrix:
-            method = command_matrix.get(command)
-            answer = await method(keyword)
+        if 50 <= user_query_object.percent < 60:
+            user_query_object.final_cmd = 'ask_again'
 
-        parse_object['answer'] = answer
-        return parse_object
+        return user_query_object
 
     @staticmethod
-    def recognize_key_word(parse_object: dict) -> dict:
+    def recognize_key_word(user_query_object: UserQueryObject) -> UserQueryObject:
         """Сбор ключевого слова соблюдая пробел"""
-        parse_object['key_word'] = ' '.join(
+        user_query_object.key_word = ' '.join(
             [
                 user_word
-                for user_word in parse_object['after_cleaning_user_txt'].split()
-                if user_word not in parse_object['original_txt']
+                for user_word in user_query_object.after_cleaning_user_txt.split()
+                if user_word not in user_query_object.original_txt
             ]
         ).strip()
-        return parse_object
+        return user_query_object
 
 
 @lru_cache(maxsize=None)
-def get_command_handler(
-        mongo_client=Depends(get_mongo_client), elastic_client: AsyncElasticsearch = Depends(get_elastic)
-) -> CommandHandler:
+def get_command_handler(mongo_client=Depends(get_mongo_client)) -> CommandHandler:
     mongo_db: AsyncIOMotorDatabase = mongo_client[settings.mongo_db]
-    return CommandHandler(mongo_db, elastic_client)
+    return CommandHandler(mongo_db)
--- voice_helper_app/src/view/api/receiving.py
+++ voice_helper_app/src/view/api/receiving.py
@@ -3,22 +3,28 @@ import orjson
 from fastapi import APIRouter, WebSocket, Depends
 
 from services.cmd_handler import CommandHandler, get_command_handler
+from services.movies_storage_handler import ElasticSeeker, get_response_maker
 
 router = APIRouter()
 
 
 # TODO:
-#  - попробовать запилить обращение по типу Алисы
-#  - разобраться почему отваливается вебсокет
-#  - убрать ненужные mount-volume (redis)
 #  - удалить ненужные пустые файлы и директории
 
 
 @router.websocket('/')
-async def websocket_endpoint(websocket: WebSocket, command_handler: CommandHandler = Depends(get_command_handler)):
+async def websocket_endpoint(
+    websocket: WebSocket,
+    command_handler: CommandHandler = Depends(get_command_handler),
+    response_maker: ElasticSeeker = Depends(get_response_maker),
+):
     await websocket.accept()
     while True:
-        user_txt = await websocket.receive_text()
-        user_txt = orjson.loads(user_txt)
-        result = await command_handler.handle_user_query(user_txt)
-        await websocket.send_text(f'{result}')
+        user_txt = orjson.loads(await websocket.receive_text())
+        user_query_object = await command_handler.handle_user_query(user_txt)
+
+        user_query_object.answer = await response_maker.execute_user_command(
+            command=user_query_object.final_cmd, keyword=user_query_object.key_word
+        )
+
+        await websocket.send_text(user_query_object.answer)
